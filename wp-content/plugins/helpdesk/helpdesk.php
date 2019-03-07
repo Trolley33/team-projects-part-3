b4660bb9755ce95c82dac068e944a5bc0f80d40d
Reece Trolley <reecetrolley@gmail.com>
2019-03-07 21:44:06 +0000

    Add composer, and import test plugins

--- wp-content/plugins/helpdesk/helpdesk.php
+++ wp-content/plugins/helpdesk/helpdesk.php
@@ -19,10 +19,22 @@ if (!defined('ABSPATH'))
 	die;
 }
 
+if (file_exists(dirname(__FILE__). '/vendor/autoload.php'))
+{
+	require_once dirname(__FILE__). '/vendor/autoload.php';
+}
+
+use Inc\Activate;
+use Inc\Deactivate;
+use Inc\Admin\AdminPages;
+
 class HelpdeskPlugin
 {
+	public $plugin;
+
 	function __construct() 
 	{
+		$this->plugin = plugin_basename( __FILE__ );
 		add_action('init', array($this, 'create_problem_post'));
 	}
 
@@ -31,30 +43,26 @@ class HelpdeskPlugin
 		add_action( 'admin_enqueue_scripts', array($this, 'enqueue') );
 
 		add_action('admin_menu', array($this, 'add_admin_pages'));
+
+		add_filter("plugin_action_links_$this->plugin", array( $this, 'settings_link'));
+	}
+
+	function settings_link ($links)
+	{
+
+		$settings_link = "<a href='admin.php?page=helpdesk'>Settings</a>";
+		array_push($links, $settings_link);
+		return $links;
 	}
 
 	function add_admin_pages ()
 	{
-		add_menu_page( "Helpdesk Page", "Helpdesk", 'manage_options', "helpdesk", array($this, 'admin_index'), '', 110 );
+		add_menu_page( "Helpdesk Page", "Helpdesk", 'manage_options', "helpdesk", array($this, 'admin_index'), 'dashicons-desktop', 110 );
 	}
 
 	function admin_index()
 	{
 		require_once plugin_dir_path( __FILE__ ) . 'templates/admin.php';
-	}
-
-	function activate()
-	{
-		// Generate custom post.
-		$this->create_problem_post();
-		// Flush DB
-		flush_rewrite_rules();
-	}
-
-	function deactivate()
-	{
-		// Flush DB
-		flush_rewrite_rules();
 	}
 
 	function create_problem_post() 
@@ -68,13 +76,25 @@ class HelpdeskPlugin
 		wp_enqueue_style( 'helpdesk_style', plugins_url('/assets/style.css', __FILE__) );
 		wp_enqueue_script( 'helpdesk_script', plugins_url('/assets/script.js', __FILE__) );
 	}
+
+	function activate()
+	{
+		Activate::activate();
+	}
+
+	function deactivate()
+	{
+		Deactivate::deactivate();
+	}
 }
 
+
+// Set up plugin
 $helpdeskPlugin = new HelpdeskPlugin();
 $helpdeskPlugin->register();
 
-// activation
+// Activate plugin function
 register_activation_hook(__FILE__, array($helpdeskPlugin, 'activate')); 
 
-// deactivate
+// Deactivate plugin function
 register_deactivation_hook(__FILE__, array($helpdeskPlugin, 'deactivate')); 
---
+++ wp-content/plugins/helpdesk/inc/Activate.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * @package Helpdesk Plugin
+ */
+namespace Inc;
+
+class Activate
+{
+	public static function activate()
+	{
+		flush_rewrite_rules();
+	}
+}
