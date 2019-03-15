ea61c593dd1eb5ecdfeaeae272f685ff0c255965
Reece Trolley <reecetrolley@gmail.com>
2019-03-15 11:41:15 +0000

    Beginnings of Custom Post Type API

---
+++ wp-content/plugins/helpdesk/inc/Base/CustomPostTypeController.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * @package Helpdesk Plugin
+ */
+namespace Inc\Base;
+
+use Inc\API\SettingsAPI;
+use Inc\API\Callbacks\AdminCallbacks;
+
+class CustomPostTypeController extends BaseController
+{
+    public $sub_pages = array();
+
+    public $callbacks;
+
+    /**
+     * Register plugin with admin menu.
+     */
+    public function register()
+    {
+        $this->settings = new SettingsAPI();
+
+        $this->callbacks = new AdminCallbacks();
+
+        add_action('init', array($this, "register_custom_post_types"));
+    }
+
+    public function register_custom_post_types()
+    {
+        register_post_type('problem',
+            [
+                'labels' => ['name'=> __("Problems"), 'singular_name'=> __("Problem")],
+                'public'=>true,
+                'has_archive'=>false,
+            ]);
+    }
+
+}
---
+++ wp-content/plugins/helpdesk/templates/problem-viewer.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: reece
+ * Date: 15/03/19
+ * Time: 11:33
+ */
