3a7bcb71d2a6de406cdaef871a3b4e005c76e410
Reece Trolley <reecetrolley@gmail.com>
2019-03-21 20:37:23 +0000

    Add new all tickets page, which loads all the tickets
    Addresses #24

--- wp-content/plugins/helpdesk/helpdesk.php
+++ wp-content/plugins/helpdesk/helpdesk.php
@@ -24,6 +24,7 @@ if (!defined('ABSPATH'))
 if (file_exists(dirname(__FILE__). '/vendor/autoload.php'))
 {
 	require_once dirname(__FILE__). '/vendor/autoload.php';
+    require_once dirname(__FILE__) . '/inc/Shortcodes/shortcode-all-tickets.php';
 }
 
 // Register functions for
