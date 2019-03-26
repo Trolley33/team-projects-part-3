36fa991de2b98f71540ea41820cc6cab31c1723f
Reece <reecetrolley@gmail.com>
2019-03-26 14:07:36 +0000

    Re-add search ability to public JS file
    Can now search through elements on 'all-tickets' page

--- wp-content/plugins/helpdesk/public/js/helpdesk-public.js
+++ wp-content/plugins/helpdesk/public/js/helpdesk-public.js
@@ -28,9 +28,56 @@
 	 * Although scripts in the WordPress core, Plugins and Themes may be
 	 * practising this, we should strive to set a better example in our own work.
 	 */
+
+	 /* This was written without jQuery prepared, so can be converted at some point */
 	// Put a page break after the label to match the rest of the style.
 	document.addEventListener('DOMContentLoaded', function () {
-		var br = document.createElement('BR');
-		document.getElementById('wpas_tags').labels[0].after(br);
+	    var br = document.createElement('BR');
+	    var tag_label = document.getElementById('wpas_tags');
+	    if (tag_label !== null)
+	    {
+	    	tag_label.labels[0].after(br);
+		}
+	   
+	    var filter = document.getElementById('wpas_filter');
+
+	    if (filter !== null) {
+	    	filter.addEventListener('keyup', 
+	    		function (event) 
+	    		{
+	    			if (event.keyCode !== 13)
+	    			{
+	    				return;
+	    			}
+
+	    			var val = this.value.toLowerCase();
+	        		var list = document.getElementById('wpas_ticketlist');
+	        		var rows = list.children[1].children;
+	        		for (let i=0; i < rows.length; i++) {
+	        			if (!rows[i].innerText.toLowerCase().includes(val))
+	        			{
+	        				rows[i].hidden = true;
+	        			}
+	        			else
+	        			{
+	        				rows[i].hidden = null;
+	        			}
+	        		}
+	    		}, false);
+
+	    	var clear_filter = document.getElementsByClassName('wpas-clear-filter');
+	    	if (clear_filter.length != 0)
+	    	{
+	    		clear_filter[0].addEventListener('click', function (event) {
+	    				filter.value = "";
+	    				var list = document.getElementById('wpas_ticketlist');
+	    				var rows = list.children[1].children;
+	    				for (let i=0; i < rows.length; i++) 
+	    				{  
+							rows[i].hidden = null;
+						}
+	    		}, false);
+	    	}
+	    }
 	}, false);
 })(jQuery);
